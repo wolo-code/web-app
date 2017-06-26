f1b4a358c315467c49b1a337478b40010d36c59e
Ujjwal Singh <ujjwalsingh@outlook.com>
2017-06-27 01:26:59 +0530

    Added App manifest

---
+++ Root/Framework/API/Config.php
@@ -0,0 +1,12 @@
+<?php
+
+function loadConfig() {
+	$fHandle = fopen(__DIR__.'\..\..\Config\Vars.tsv', 'r');
+	while(($tsvLine = fgetcsv($fHandle, 0, "\t")) !== FALSE) {
+		$config[$tsvLine[0]] = $tsvLine[1];
+	}
+
+  return $config;
+}
+
+?>
--- Root/HTML/Template/Base.php
+++ Root/HTML/Template/Base.php
@@ -3,12 +3,15 @@
 <head>
 	<meta http-equiv="X-UA-Compatible" content="chrome=1" />
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta name="title" content="<?php echo $config['project_title'] ?>" />
+	<meta itemprop="name" content="<?php echo $config['project_title'] ?>" />
 	<meta charset='utf-8' >
 	<meta name='robots' content='noindex' >
-	<meta name='author' content='ujjwalsingh@outlook.com' >
+	<meta name="author" content="<?php echo $config['author'] ?>" />
 	<meta name='viewport' content='initial-scale=1.0' >
 	<link rel='shortcut icon' type='image/x-icon' href='/favicon.ico' >
-	<title>WCode location</title>
+	<link rel='manifest' href='/manifest.json' >
+	<link href="<?php echo $config['base_url']; if($id != 'root') echo '/'.$id ?>" rel='canonical' >
 	<?php
 		if($bPublish) {
 	?>
@@ -22,10 +25,8 @@
 </head>
 <body>
 <?php
-	require '../../HTML/Component/Root.php';
-?>
-<?php
 	require '../JS/Fragment/JS.php';
+	require '../../HTML/Component/Root.php';
 ?>
 </body>
 </html>

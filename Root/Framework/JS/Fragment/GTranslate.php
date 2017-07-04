0d5f3fee98009ef144db8d5e6a03178b36245a75
Ujjwal Singh <ujjwalsingh@outlook.com>
2017-07-04 17:17:42 +0530

    Removed unused fragments

--- Root/Framework/JS/Fragment/GTranslate.php
+++
@@ -1,5 +0,0 @@
-<script type='text/javascript'>
-	function googleTranslateElementInit() {
-		new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.HORIZONTAL, autoDisplay: false, gaTrack: true <?php if($bPublish) echo ", gaId: '".$config['google_analytics_id']."'"; ?>}, 'google_translate_element');
-	}
-</script>

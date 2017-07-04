79c8480cc2c4e6c82f4b2d120acdd83e8c73022e
Ujjwal Singh <ujjwalsingh@outlook.com>
2017-07-04 18:13:30 +0530

    Fixed config

--- Root/Framework/HTML/Page.php
+++ Root/Framework/HTML/Page.php
@@ -1,6 +1,9 @@
 <?php
 	require_once '../API/API.php';
+	require_once '../API/Config.php';
 
+	$config = loadConfig();
+	
 	if( isset($_GET['mode']) && ($_GET['mode'] === "publish") )
 		$bPublish = TRUE;
 	else

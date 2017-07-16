4aa45ede1e4e6f9266a056befe4bc18128acf315
Ujjwal Singh <ujjwalsingh@outlook.com>
2017-07-16 17:44:47 +0530

    Corrected

--- Root/Test/Test.js
+++ Root/Test/Test.js
@@ -1,12 +1,4 @@
+var curPos = { "lat": 12.9, "lng": 77.5 };
 
-//var city_begin = { "lat": 10.767, "lng": 12.76876 };
-//var curPos = { "lat": 12.67876, "lng": 14.676876 };
-
-var city_begin = { "lat": 12.831878, "lng": 80.062018 };
-var curPos = { "lat": 12.909612, "lng": 80.227136 };
-
-var code = encode(city_begin, curPos);
-//decode(city_begin, code);
-
-var code = encode(city_begin, curPos);
+var code = encode(curPos);
 decode(code.split(" "));

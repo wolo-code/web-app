fff4738e9a77cc5acd8ab92f94e8370d2e43b3bd
Ujjwal Singh <ujjwalsingh@outlook.com>
2017-07-25 03:45:32 +0530

    Updated test code js

--- Root/Test/Code_2.js
+++ Root/Test/Code_2.js
@@ -1,40 +1,52 @@
-var factor_lat_1 = 111.321543;
-var factor_lat_2 = factor_lat_1 / 32;
-var factor_long_1 = (Math.cos(13.057556) * 111.321543);
-var factor_long_2 = factor_long_1 / 32;
+var span = 0.5;
+var n = 1024;
+var d = span/n;
+
+function encodeData(value) {
+	return Math.floor(value/d);
+}
+
+function decodeData(data) {
+	return data*d;
+}
 
 function encode_(city_begin, position) {
-	var lat_diff = (position.lat - city_begin.lat) * factor_lat_1;
-	var lat_diff_1 = Math.floor(lat_diff);
-	var lat_diff_2 = Math.round((lat_diff - lat_diff_1) * factor_lat_2);
-	var long_diff = (position.lng - city_begin.lng) * factor_long_1;
-	var long_diff_1 = Math.floor(long_diff);
-	var long_diff_2 = Math.round((long_diff - long_diff_1) * factor_long_2);
-	var word_index_1 = (lat_diff_1 << 5) | (long_diff_1);
-	var word_index_2 = (lat_diff_2 << 5) | (long_diff_2);
-	var code = wordList[word_index_1] + " " + wordList[word_index_2];
-
-	logCode(code, lat_diff_1 + " " + lat_diff_2 + " " + long_diff_1 + " " + long_diff_2);
-	return code;
+	var code = encode__(city_begin, position);
+	var wcode = wordList[word_index_1] + " " + wordList[word_index_2];
+	logCode(wcode, lat_diff + " " + lng_diff);
+	return wcode;
 }
 
 function decode_(city_begin, code) {
-	var word_index_1 = wordList.indexOf(code[0]);
-	var word_index_2 = wordList.indexOf(code[1]);
-	var lat_diff_1 = word_index_1 >> 5;
-	var long_diff_1 = word_index_1 & 0x1F;
-	var lat_diff_2 = word_index_2 >> 5;
-	var long_diff_2 = word_index_2 & 0x1F;
-	var lat = (lat_diff_1 + Math.round(lat_diff_2 / factor_lat_2)) / factor_lat_1 + city_begin.lat;
-	var long = (long_diff_1 + Math.round(long_diff_2 / factor_long_2))  / factor_long_1 + city_begin.lng;
-
-	logDifference(lat, long, lat_diff_1 + " " + lat_diff_2 + " " + long_diff_1 + " " + long_diff_2);
+	var latLng = decode__( city_begin, [wordList.indexOf(code[0]), wordList.indexOf(code[1])] );
+	logDifference(latLng.lat, latLng.lng, lat_diff + " " + lng_diff);
 	return {
 		lat: (function() {
-			return lat
+			return latLng.lat
 		})(),
 		lng: (function() {
-			return long
+			return latLng.lng
 		})()
 	};
+}
+
+function encode__(city_begin, position) {
+	lat_diff = encodeData(position.lat - city_begin.lat);
+	lng_diff = encodeData(position.lng - city_begin.lng);
+	word_index_1 = lat_diff;
+	word_index_2 = lng_diff;
+
+	var code = [word_index_1, word_index_2];
+	return code;
+}
+
+function decode__(city_begin, code) {
+	lat_diff_bin = code[0];
+	lng_diff_bin = code[1];
+	var lat_diff = decodeData(lat_diff_bin);
+	var lng_diff = decodeData(lng_diff_bin);
+	lat = lat_diff + city_begin.lat;
+	lng = lng_diff + city_begin.lng;
+
+	return({"lat":lat, "lng":lng});
 }
--- Root/Test/Code_3.js
+++ Root/Test/Code_3.js
@@ -1,50 +1,53 @@
-var factor_lat_1 = 100;
-var factor_lat_2 = 3;
-var factor_lat_3 = 3;
-var factor_lng_1 = 100;
-var factor_lng_2 = 3;
-var factor_lng_3 = 3;
+var span = 0.5;
+var n = 32768;
+var d = span/n;
+
+function encodeData(value) {
+	return Math.floor(value/d);
+}
+
+function decodeData(data) {
+	return data*d+d/2;
+}
 
 function encode_(city_begin, position) {
-	lat_diff = (position.lat - city_begin.lat) * factor_lat_1;
-	lat_diff_1 = Math.floor(lat_diff);
-	lat_diff_2_ = (lat_diff - lat_diff_1) * factor_lat_2;
-	lat_diff_2 = Math.floor(lat_diff_2_);
-	lat_diff_3 = Math.round((lat_diff_2_ - lat_diff_2) * factor_lat_3);
-	lng_diff = (position.lng - city_begin.lng) * factor_lng_1;
-	lng_diff_1 = Math.floor(lng_diff);
-	lng_diff_2_ = (lng_diff - lng_diff_1) * factor_lng_2;
-	lng_diff_2 = Math.floor(lng_diff_2_);
-	lng_diff_3 = Math.round((lng_diff_2_ - lng_diff_2) * factor_lng_3);
-	word_index_1 = (lat_diff_1 << 5) | (lng_diff_1);
-	word_index_2 = (lat_diff_2 << 5) | (lng_diff_2);
-	word_index_3 = (lat_diff_3 << 5) | (lng_diff_3);
-	code = wordList[word_index_1] + " " + wordList[word_index_2] + " " + wordList[word_index_3];
-
-	logCode(code, lat_diff_1 + " " + lat_diff_2 + " " + lat_diff_3 + " " + lng_diff_1 + " " + lng_diff_2 + " " + lng_diff_3);
-	return code;
+	var code = encode__(city_begin, position);
+	var wcode = wordList[word_index_1] + " " + wordList[word_index_2] + " " + wordList[word_index_3];
+	logCode(wcode, lat_diff + " " + lng_diff);
+	return wcode;
 }
 
 function decode_(city_begin, code) {
-	word_index_1 = wordList.indexOf(code[0]);
-	word_index_2 = wordList.indexOf(code[1]);
-	word_index_3 = wordList.indexOf(code[2]);
-	lat_diff_1 = word_index_1 >> 5;
-	lng_diff_1 = word_index_1 & 0x1F;
-	lat_diff_2 = word_index_2 >> 5;
-	lng_diff_2 = word_index_2 & 0x1F;
-	lat_diff_3 = word_index_3 >> 5;
-	lng_diff_3 = word_index_3 & 0x1F;
-	lat = (lat_diff_1 + (lat_diff_2 + (lat_diff_3 / factor_lat_3)) / factor_lat_2) / factor_lat_1 + city_begin.lat;
-	lng = (lng_diff_1 + (lng_diff_2 + (lng_diff_3 / factor_lng_3)) / factor_lng_2) / factor_lng_1 + city_begin.lng;
-
-	logDifference(lat, lat_diff_1 + " " + lat_diff_2 + " " + lat_diff_3 + " " + lng_diff_1 + " " + lng_diff_2 + " " + lng_diff_3);
+	var latLng = decode__( city_begin, [wordList.indexOf(code[0]), wordList.indexOf(code[1]), wordList.indexOf(code[2])] );
+	logDifference(latLng.lat, latLng.lng, lat_diff + " " + lng_diff);
 	return {
 		lat: (function() {
-			return lat
+			return latLng.lat
 		})(),
 		lng: (function() {
-			return lng
+			return latLng.lng
 		})()
 	};
+}
+
+function encode__(city_begin, position) {
+	lat_diff = encodeData(position.lat - city_begin.lat);
+	lng_diff = encodeData(position.lng - city_begin.lng);
+	word_index_1 = lat_diff >> 5
+	word_index_2 = lng_diff >> 5;
+	word_index_3 = (lat_diff & 0x001F) << 5 | (lng_diff & 0x001F);
+
+	var code = [word_index_1, word_index_2, word_index_3];
+	return code;
+}
+
+function decode__(city_begin, code) {
+	lat_diff_bin = code[0] << 5 | code[3] >> 5;
+	lng_diff_bin = code[1] << 5 | code[3] & 0x001F;
+	var lat_diff = decodeData(lat_diff_bin);
+	var lng_diff = decodeData(lng_diff_bin);
+	lat = lat_diff + city_begin.lat;
+	lng = lng_diff + city_begin.lng;
+
+	return({"lat":lat, "lng":lng});
 }

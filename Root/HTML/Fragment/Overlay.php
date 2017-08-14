576a5cc86fce5ef5f9bbc3c959aa485ec5deeca9
Ujjwal Singh <ujjwalsingh@outlook.com>
2017-08-14 13:42:47 +0530

    Overlay

--- Root/HTML/Fragment/Overlay.php
+++ Root/HTML/Fragment/Overlay.php
@@ -7,24 +7,26 @@
 			<span class='image'><?php echo file_get_contents('../../Resource/Close.svg'); ?></span>
 		</div>
 		<h1>
-			Note
+			WCode location
 		</h1>
 		<h2>
-			Status
+			About
 		</h2>
 		<ol>
-			<li>This is a beta demo of word based geo addressing</li>
-			<li>The first word specifies the city, the other three words are from a limited dictionary of 1024 easy common nouns</li>
-			<li>Limited to 'Bangalore', 'India'</li>
+			<li>This is a beta version of word based geocoding system</li>
+			<li>The first part specifies the city, the last three words are from a limited dictionary of 1024 easy common nouns</li>
+			<li>Incase if your city is not in the database, please send a request</li>
 			<li>Your precise location will NOT be recorded</li>
 			<li>Tested with Google Chrome</li>
 		</ol>
+		<br>
+		<div class="center"><button id='overlay_enter' type='button'>Start &#x25B7;</button></div>
 		<h2>
 			Steps
 		</h2>
 		<ol>
-			<li>Click to generate WCode for that location</li>
-			<li>Enter a WCode and press 'Decode' to go to that location</li>
+			<li>Point a location to generate the corresponding WCode</li>
+			<li>Enter a WCode and hit 'Decode' to go to that location</li>
 		</ol>
 		<h2>
 			Known issues
@@ -32,7 +34,6 @@
 		<ol>
 			<li>The server roundtrip induces a delay. You may have to wait a few seconds for the result to show up</li>
 		</ol>
-		<div class="center"><button id='overlay_enter' type='button'>Enter</button></div>
 		<div id='contact'>
 			For any related query contact <a class='link' href='https://wcodes.org/about_me'>me</a> at: <a class="link" href="mailto:ujjwalsingh@outlook.com?subject=WCode-location">ujjwalsingh@outlook.com</a>
 		</div>

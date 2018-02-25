9ba908bdde391dcf826d56b2a17c576fba89e1ee
Ujjwal Singh <ujjwalsingh@outlook.com>
2018-02-26 00:15:44 +0530

    Fixed 'Overlay' php include issue

--- Root/HTML/Fragment/Overlay.php
+++ Root/HTML/Fragment/Overlay.php
@@ -8,7 +8,7 @@
 		</h1>
 <?php
 	echo file_get_contents('../../HTML/Fragment/Info_intro.html');
-	echo file_get_contents('../../HTML/Fragment/Info_full.php');
+	require '../../HTML/Fragment/Info_full.php';
 ?>
 		<div id='agency'>
 			by <a class='link' href='https://wcodes.org/about_me'>Ujjwal Singh</a>

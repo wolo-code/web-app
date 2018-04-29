2e9469d115396871e34a832481d17e5a0bc0b796
Ujjwal Singh <ujjwalsingh@outlook.com>
2018-04-30 05:10:05 +0530

    Fixed stray 'requried' attribute and button html

--- Root/HTML/Component/Console/Index.php
+++ Root/HTML/Component/Console/Index.php
@@ -38,9 +38,6 @@
 				<div id='data_lat'>Lat</div>
 				<div id='data_lng'>Long</div>
 			</div>
-				<input id='city_lat' type='hidden' required >
-				<input id='city_lng' type='hidden' required >
-				<botton id='submit_city_button' class='control' type='button' value='Submit' tabindex='7'>Submit</button>
 		</div>
 		<div id='address_text'>
 			<div id='address_text_label'>Address</div>
@@ -50,9 +47,12 @@
 		</div>
 		<input id='pac-input' type='text' placeholder='Search' tabindex='3' >
 		<form id='city_submit_panel'>
+			<input id='city_lat' type='hidden' >
+			<input id='city_lng' type='hidden' >
 			<input id='city_name' type='text' placeholder='Name' required tabindex='4' >
 			<input id='city_group' type='text' placeholder='Group' required tabindex='5' >
 			<input id='city_country' type='text' placeholder='Country' required tabindex='6' >
+			<input id='submit_city_button' class='control' type='submit' value='Submit' tabindex='7'>
 		</form>
 		<div id='notification_bottom' class='hide'></div>
 	</div>

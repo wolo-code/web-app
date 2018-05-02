ed9a5d9cfa81c35b275ddfbbcba5517cb6a0d4c3
Ujjwal Singh <ujjwalsingh@outlook.com>
2018-05-02 21:00:29 +0530

    Info_common.html extracted

---
+++ Root/HTML/Fragment/Info_intro.php
@@ -0,0 +1,8 @@
+<div id='info_intro'>
+	<?php echo file_get_contents('../../HTML/Fragment/Info_common.html'); ?>
+	<div class='message_dialog_control_container'>
+		<div class='message_dialog_control'>
+			<button id='info_intro_close_button' type='button'>Start &#x25B7;</button>
+		</div>
+	</div>
+</div>
--- Root/HTML/Fragment/Overlay.php
+++ Root/HTML/Fragment/Overlay.php
@@ -7,7 +7,7 @@
 			WCode location
 		</h1>
 <?php
-	echo file_get_contents('../../HTML/Fragment/Info_intro.html');
+	require '../../HTML/Fragment/Info_intro.php';
 	require '../../HTML/Fragment/Info_full.php';
 ?>
 		<div id='agency'>

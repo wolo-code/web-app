b6889e8eee20ca61432f2ef55674de020b703df5
Ujjwal Singh <ujjwalsingh@outlook.com>
2018-05-13 22:54:49 +0530

    Changed id references to document.getElementById

--- Root/JS/Component/Root/Script.js
+++ Root/JS/Component/Root/Script.js
@@ -1,21 +1,23 @@
 window.onload = init;
 
 function init() {
-	overlay.addEventListener('click', hideOverlay);
-	overlay_message_close.addEventListener('click', hideOverlay);
-	info.addEventListener('click', showOverlay);
-	no_city_message_close.addEventListener('click', hideNoCityMessage);
-	locate_right_message_close.addEventListener('click', hideLocateRightMessage);
-	locate_right_message_yes.addEventListener('click', locateRight_grant);
-	locate_right_message_no.addEventListener('click', locateRight_deny);
-	no_city_submit_yes.addEventListener('click', noCity_add);
-	no_city_submit_no.addEventListener('click', noCity_cancel);
-	no_city_submit_wait_continue.addEventListener('click', noCityWait_continue);
-	no_city_submit_wait_stop.addEventListener('click', noCityWait_stop);
-	notification_top.addEventListener('click', tryDefaultCity);
-	copy_wcode_message_close.addEventListener('click', hideCopyWcodeMessage);
-	copy_wcode_submit_yes.addEventListener('click', copyWcodeFull);
-	copy_wcode_submit_no.addEventListener('click', copyWcodeCode);
+	document.getElementById('overlay').addEventListener('click', hideOverlay);
+	document.getElementById('overlay_message_close').addEventListener('click', hideOverlay);
+	document.getElementById('info').addEventListener('click', showOverlay);
+	document.getElementById('no_city_message_close').addEventListener('click', hideNoCityMessage);
+	document.getElementById('locate_right_message_close').addEventListener('click', hideLocateRightMessage);
+	document.getElementById('locate_right_message_yes').addEventListener('click', locateRight_grant);
+	document.getElementById('locate_right_message_no').addEventListener('click', locateRight_deny);
+	document.getElementById('no_city_submit_yes').addEventListener('click', noCity_add);
+	document.getElementById('no_city_submit_no').addEventListener('click', noCity_cancel);
+	document.getElementById('no_city_submit_wait_continue').addEventListener('click', noCityWait_continue);
+	document.getElementById('no_city_submit_wait_stop').addEventListener('click', noCityWait_stop);
+	document.getElementById('notification_top').addEventListener('click', tryDefaultCity);
+	document.getElementById('copy_wcode_message_close').addEventListener('click', hideCopyWcodeMessage);
+	document.getElementById('copy_wcode_submit_yes').addEventListener('click', copyWcodeFull);
+	document.getElementById('copy_wcode_submit_no').addEventListener('click', copyWcodeCode);
+	document.getElementById('address_text_close').addEventListener('click', hideAddress);
+	document.getElementById('address_text_copy').addEventListener('click', copyAddress);
 }
 
 function showAndCopy(message) {

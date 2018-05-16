9f378edbe074ae89398c6599df96ed244547c90c
Ujjwal Singh <ujjwalsingh@outlook.com>
2018-05-17 05:16:38 +0530

    Combining script into 1

--- Root/Framework/JS/Component.php
+++ Root/Framework/JS/Component.php
@@ -4,6 +4,8 @@
 
 	header('Content-Type: text/javascript');
 
+	includeDir('../JS/', $INCLUDE_TYPE_JS, $INCLUDE_MODE_EMBED, 'base');
+	includeDir('../../JS/', $INCLUDE_TYPE_JS, $INCLUDE_MODE_EMBED, 'base');
 	$component_id = getOrigCallComponent();
 	includeDir('../../JS/Component/'.$component_id.'/', $INCLUDE_TYPE_JS, $INCLUDE_MODE_EMBED, 'base');
 	require '../../JS/SVGs.php';

af71a17b627d4b0d7e2defb3fa3bea0ac06cfe66
Ujjwal Singh <ujjwalsingh@outlook.com>
2018-11-06 12:08:28 +0530

    var to const

--- Root/JS/SVGs.php
+++ Root/JS/SVGs.php
@@ -1,5 +1,5 @@
 <?php require_once '../API/IncludeSVG.php' ?>
-var svg_address = "<?php getSVG('', 'address'); ?>";
-var svg_copy = "<?php getSVG('', 'copy'); ?>";
-var svg_link = "<?php getSVG('', 'link'); ?>";
-var svg_map = "<?php getSVG('', 'map'); ?>";
+const svg_address = "<?php getSVG('', 'address'); ?>";
+const svg_copy = "<?php getSVG('', 'copy'); ?>";
+const svg_link = "<?php getSVG('', 'link'); ?>";
+const svg_map = "<?php getSVG('', 'map'); ?>";

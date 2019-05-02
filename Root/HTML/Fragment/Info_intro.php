f3ee7f99263b7d0457ea207512be4ff81075c67b
Ujjwal Singh <ujjwalsingh@outlook.com>
2019-05-02 20:01:18 +0530

    Hollow to filled arrow

--- Root/HTML/Fragment/Info_intro.php
+++ Root/HTML/Fragment/Info_intro.php
@@ -2,7 +2,7 @@
 	<?php echo file_get_contents('../../HTML/Fragment/Info_common.html'); ?>
 	<div class='message_dialog_control_container'>
 		<div class='message_dialog_control'>
-			<button id='info_intro_close_button' class='button_highlight' type='button'>Start &#x25B7;</button>
+			<button id='info_intro_close_button' class='button_highlight' type='button'>Start &#x25BA;</button>
 		</div>
 	</div>
 </div>

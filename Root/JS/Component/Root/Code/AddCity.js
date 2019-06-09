7bb80e27c5d62d7d82aa0abb6030f31f7cc930e8
Ujjwal Singh <ujjwalsingh@outlook.com>
2019-06-09 20:26:39 +0530

    Code.js AddCity externalized

---
+++ Root/JS/Component/Root/Code/AddCity.js
@@ -0,0 +1,28 @@
+function addCity(gp_id, callback) {
+
+	var http = new XMLHttpRequest();
+	http.open('POST', FUNCTIONS_BASE_URL+'/'+'add-city', true);
+
+	http.setRequestHeader('Content-type', 'application/json');
+	http.setRequestHeader('version', '1');
+	http.requestId = ++curAddCityRequestId;
+
+	wait_loader.classList.remove('hide');
+	http.onreadystatechange = function() {
+		if(http.readyState == 4 && http.status == 200) {
+			if(http.requestId == curAddCityRequestId) {
+				callback(JSON.parse(http.responseText).added);
+				wait_loader.classList.add('hide');
+			}
+		}
+	}
+
+	http.send( stringifyAddCityData(gp_id) );
+
+}
+
+function stringifyAddCityData(gp_id) {
+	var object = {};
+	object['place_id'] = gp_id;
+	return JSON.stringify(object);
+}

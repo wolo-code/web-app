c8957a97474a32972acd4a25b6c20524803ffc91
Ujjwal Singh <ujjwalsingh@outlook.com>
2019-09-16 23:32:20 +0530

    Added sentry hash

--- Root/Framework/JS/Fragment/Sentry_version.php
+++ Root/Framework/JS/Fragment/Sentry_version.php
@@ -1,1 +1,1 @@
-src='https://browser.sentry-cdn.com/<?php echo $config['sentry_version'] ?>/bundle.min.js' crossorigin='anonymous'
+src='https://browser.sentry-cdn.com/<?php echo $config['sentry_version'] ?>/bundle.min.js' integrity='<?php echo $config['sentry_hash'] ?>' crossorigin='anonymous'

3e8ee216f9cc84ebc6c06266446b8c376009a27b
Ujjwal Singh <ujjwalsingh@outlook.com>
2019-11-02 13:06:46 +0530

    Externalized common getAddressCity code

--- Root/JS/Component/Console/Form.js
+++ Root/JS/Component/Console/Form.js
@@ -1,11 +1,11 @@
-function fillForm(gpid, lat, lng, city, accent, group, country) {
-	city_gpid.value = gpid;
-	city_lat.value = lat;
-	city_lng.value = lng;
-	city_name.value = city;
-	city_accent.value = accent;
-	city_group.value = group;
-	city_country.value = country;
+function fillForm(address_object) {
+	city_gpid.value = address_object.gp_id;
+	city_lat.value = address_object.city_lat;
+	city_lng.value = address_object.city_lng;
+	city_name.value = address_object.city_name;
+	city_accent.value = address_object.city_accent;
+	city_group.value = address_object.group;
+	city_country.value = address_object.country;
 }
 
 function clearForm() {

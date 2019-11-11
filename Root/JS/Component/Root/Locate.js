3c7f523e9983945a17a311f90b8e3d4a3f67dc40
Ujjwal Singh <ujjwalsingh@outlook.com>
2019-11-11 11:36:29 +0530

    Removed unused arguments for handleLocationError()

--- Root/JS/Component/Root/Locate.js
+++ Root/JS/Component/Root/Locate.js
@@ -86,11 +86,11 @@ function locateExec(failure) {
 		);
 	} else {
 		// Browser doesn't support Geolocation
-		handleLocationError(false, infoWindow, map.getCenter());
+			handleLocationError(false);
 	}
 }
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+function handleLocationError(browserHasGeolocation) {
 	showNotification(browserHasGeolocation ?
 												'Error: The Geolocation service failed' :
 												'Error: Your browser doesn\'t support geolocation');

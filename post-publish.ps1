aa9c363a7e0208ffb757beccc8334159f838ff85
Ujjwal Singh <ujjwalsingh@outlook.com>
2020-01-26 15:53:45 +0530

    Added js file link versioning by CRC

---
+++ post-publish.ps1
@@ -0,0 +1,13 @@
+. crc32.ps1
+
+$crc = get-crc32 ./public/root.js
+mv ./public/root.js ./public/root-$crc.js
+$file = ".\public\index.html"
+$text = (Get-Content -Path $file -ReadCount 0) -join "`n"
+$text -replace '<script src="/root', ('<script src="/root-'+$crc) | Set-Content -Path $file
+
+$crc = get-crc32 ./public/console.js
+mv ./public/console.js ./public/console-$crc.js
+$file = ".\public\console.html"
+$text = (Get-Content -Path $file -ReadCount 0) -join "`n"
+$text -replace '<script src="/console', ('<script src="/console-'+$crc) | Set-Content -Path $file

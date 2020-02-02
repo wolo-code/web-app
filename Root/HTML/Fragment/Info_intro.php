a318062ce10b8af96146bb76823f43f9f176061a
Ujjwal Singh <ujjwalsingh@outlook.com>
2020-02-02 16:13:58 +0530

    Button front indicator - text code replaced with svg

--- Root/HTML/Fragment/Info_intro.php
+++ Root/HTML/Fragment/Info_intro.php
@@ -2,7 +2,7 @@
 	<?php echo file_get_contents('../../HTML/Fragment/Info_common.html'); ?>
 	<div class='message_dialog_control_container'>
 		<div class='message_dialog_control'>
-			<button id='info_intro_close_button' class='button_highlight' type='button'>Start &#x25BA;</button>
+			<button id='info_intro_close_button' class='button_highlight' type='button'>Start <span class="image front_arrow_white"><?php includeSVG('', 'Front'); ?></span></button>
 		</div>
 	</div>
 </div>

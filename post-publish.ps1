996e4099f1e6c7e526d25f6778ace29280b855c0
Ujjwal Singh <ujjwalsingh@outlook.com>
2020-02-13 00:57:01 +0530

    Corrected script file references

--- post-publish.ps1
+++ post-publish.ps1
@@ -2,12 +2,22 @@
 
 $crc = get-crc32 ./public/root.js
 mv ./public/root.js ./public/root-$crc.js
+
 $file = ".\public\index.html"
 $text = (Get-Content -Path $file -ReadCount 0) -join "`n"
 $text -replace '<script src="/root', ('<script src="/root-'+$crc) | Set-Content -Path $file
 
+$file = ".\public\sw.js"
+$text = (Get-Content -Path $file -ReadCount 0) -join "`n"
+$text -replace './root', ('./root-'+$crc) | Set-Content -Path $file
+
 $crc = get-crc32 ./public/console.js
 mv ./public/console.js ./public/console-$crc.js
+
 $file = ".\public\console.html"
 $text = (Get-Content -Path $file -ReadCount 0) -join "`n"
 $text -replace '<script src="/console', ('<script src="/console-'+$crc) | Set-Content -Path $file
+
+$file = ".\public\sw.js"
+$text = (Get-Content -Path $file -ReadCount 0) -join "`n"
+$text -replace './console', ('./console-'+$crc) | Set-Content -Path $file

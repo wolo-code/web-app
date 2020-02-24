28e0518d4822253a7a03a301b7255616f7237053
Ujjwal Singh <ujjwalsingh@outlook.com>
2020-02-25 01:13:47 +0530

    Added cookie notice

--- Root/HTML/Fragment/Info_intro.php
+++ Root/HTML/Fragment/Info_intro.php
@@ -1,5 +1,9 @@
 <div id='info_intro'>
 	<?php echo file_get_contents('../../HTML/Fragment/Info_common.html'); ?>
+	<p>
+		This web app requires anonymous cookies and various third-party services to function properly<br>
+		To continue using Wolo codes you must consent to its <a href='./cookie'>cookie</a> & <a href='/.privacy'>privacy</a> policies.
+	</p>
 	<div class='message_dialog_control_container'>
 		<div class='message_dialog_control'>
 			<button id='info_intro_close_button' class='button_highlight' type='button'>Start <span class="image front_arrow_white"><?php includeSVG('', 'Front'); ?></span></button>

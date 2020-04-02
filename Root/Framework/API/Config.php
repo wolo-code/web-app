90171ef5e3475a74ea6a93de259ecbd48aee1c79
Ujjwal Singh <ujjwalsingh@outlook.com>
2020-04-02 17:19:53 +0530

    Provision for common vars.tsv other than _prod and _dev

--- Root/Framework/API/Config.php
+++ Root/Framework/API/Config.php
@@ -2,10 +2,18 @@
 
 function loadConfig() {
 	global $variant; 
+	
+	$fHandle = fopen(__DIR__.'\..\..\Config\Vars.tsv', 'r');
+	while(($tsvLine = fgetcsv($fHandle, 0, "\t")) !== FALSE) {
+		$config[$tsvLine[0]] = $tsvLine[1];
+	}
+	fclose($fHandle);
+	
 	$fHandle = fopen(__DIR__.'\..\..\Config\Vars_'.$variant.'.tsv', 'r');
 	while(($tsvLine = fgetcsv($fHandle, 0, "\t")) !== FALSE) {
 		$config[$tsvLine[0]] = $tsvLine[1];
 	}
+	fclose($fHandle);
 
 	return $config;
 }

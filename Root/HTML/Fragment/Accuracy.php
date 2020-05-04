972c6b790207f2641a2d7450ecf9aa3450830eb6
Ujjwal Singh <ujjwalsingh@outlook.com>
2020-05-04 07:38:53 +0530

    Corrected typo: span not closed

--- Root/HTML/Fragment/Accuracy.php
+++ Root/HTML/Fragment/Accuracy.php
@@ -2,7 +2,8 @@
 	<div>
 		<span id='accuracy_indicator'>&nbsp;</span>
 		<span>accuracy</span> <span id='accuracy_meter'></span> <span>m</span>
-		<span id='proceed_container'>&nbsp<span>
-		<span id='proceed_button' class='control'>Proceed</span>
+		<span id='proceed_container'>&nbsp
+			<span id='proceed_button' class='control'>Proceed</span>
+		</span>
 	</div>
 </div>

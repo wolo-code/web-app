8e4475e8eeb8b0a8eb2f88a5699740d9a0d2312b
Ujjwal Singh <ujjwalsingh@outlook.com>
2020-05-04 07:39:23 +0530

    Replaced nbsp with padding

--- Root/HTML/Fragment/Accuracy.php
+++ Root/HTML/Fragment/Accuracy.php
@@ -1,8 +1,8 @@
 <div id='accuracy_container' class="center-container-abs hide">
 	<div>
 		<span id='accuracy_indicator'>&nbsp;</span>
-		<span>accuracy</span> <span id='accuracy_meter'></span> <span>m</span>
-		<span id='proceed_container'>&nbsp
+		<span>accuracy</span> <span id='accuracy_meter'></span> <span id='accuracy_meter_unit'>m</span>
+		<span id='proceed_container'>
 			<span id='proceed_button' class='control'>Proceed</span>
 		</span>
 	</div>

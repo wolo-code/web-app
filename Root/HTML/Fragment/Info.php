22c5f82fbe0dfa0c2f4761b7b626f1d3467d6cb6
Ujjwal Singh <ujjwalsingh@outlook.com>
2020-05-10 20:50:22 +0530

    overlay changed to info for info message

---
+++ Root/HTML/Fragment/Info.php
@@ -0,0 +1,21 @@
+<div id='info_message' class='hide'>
+	<div id='overlay_message_close' class="message_dialog_close control">
+		<span class='image'><?php includeSVG('', 'Close'); ?></span>
+	</div>
+	<div id='logo_info'>
+		<a id=logo_wolo_info href='//wcodes.org' tabindex='1'>
+			<span class='image'><?php includeSVG('', 'logo_wolo'); ?></span>
+		</a>
+		<a id=logo_codes_info href='//wolo.codes' tabindex='2'>
+			<span class='image'><?php includeSVG('', 'logo_codes'); ?></span>
+		</a>
+	</div>
+	<?php echo file_get_contents('../../HTML/Fragment/Info_common.html'); ?>
+	<?php
+		require '../../HTML/Fragment/Info_intro.php';
+		require '../../HTML/Fragment/Info_full.php';
+	?>
+	<div id='agency'>
+		by <a class='link' href='https://wcodes.org/about_me'>Ujjwal Singh</a>
+	</div>
+</div>

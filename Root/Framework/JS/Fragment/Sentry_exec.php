54419b7cb3cb34596c7499262fb605ece2d55f33
Ujjwal Singh <ujjwalsingh@outlook.com>
2020-05-11 02:14:28 +0530

    Updated sentry and seperated environment variants

--- Root/Framework/JS/Fragment/Sentry_exec.php
+++ Root/Framework/JS/Fragment/Sentry_exec.php
@@ -1,1 +1,1 @@
-Sentry.init({ dsn: 'https://<?php echo $config['sentry_key'] ?>@sentry.io/<?php echo $config['sentry_id'] ?>' });
+Sentry.init({ dsn: 'https://<?php echo $config['sentry_key'] ?>@<?php echo $config['sentry_ingest_sub_id'] ?>.ingest.sentry.io/<?php echo $config['sentry_id'] ?>' });

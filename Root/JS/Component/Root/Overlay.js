64c7415f9312f80acf13667d7982a4559e8a5a2d
Ujjwal Singh <ujjwalsingh@outlook.com>
2020-06-13 04:00:02 +0530

    Fixed overlay lingering bug

--- Root/JS/Component/Root/Overlay.js
+++ Root/JS/Component/Root/Overlay.js
@@ -1,13 +1,13 @@
 function hideOverlay(e) {
-	if(!e.classList.contains('hide')) {
-		document.getElementById('overlay').classList.add('hide');
+	if(!e.classList.contains('hide'))
 		e.classList.add('hide');
-	}
+	if(!document.getElementById('overlay').classList.contains('hide'))
+		document.getElementById('overlay').classList.add('hide');			
 }
 
 function showOverlay(e) {
-	if(e.classList.contains('hide')) {
+	if(document.getElementById('overlay').classList.contains('hide'))
 		document.getElementById('overlay').classList.remove('hide');
+	if(e.classList.contains('hide'))
 		e.classList.remove('hide');
-	}
 }

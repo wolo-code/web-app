0a252229049e4ecb1d7eca58ac192b8067c4cc23
Ujjwal Singh <ujjwalsingh@outlook.com>
2020-06-24 22:53:25 +0530

    Rearranged info_full copy

--- Root/HTML/Fragment/Info_full.php
+++ Root/HTML/Fragment/Info_full.php
@@ -4,9 +4,6 @@
 			<button id='info_full_close_button' class='button_highlight' type='button'><span class='indicator'><span class='image'><?php includeSVG('', 'Back'); ?></span></span> return</button>
 		</div>
 	</div>
-	<ul id='info_list'>
-		<li>Tested to work with Google Chrome</li>
-	</ul>
 	
 	<div id='download-unified-url' class='center'>
 		<span>app:</span> <a class='link' href="https://wolo.codes/get">wolo.codes/get</a>
@@ -14,6 +11,19 @@
 	<div id='download-android-bottom' class="download coming-soon center">
 		<a href='https://play.google.com/store/apps/details?id=codes.wolo.droid' id='site-google-play' onclick="trackOutboundLink('wolo-appandroid', 'https://play.google.com/store/apps/details?id=codes.wolo.droid')"><span class='image'><?php includeSVG('', 'Google-Play-badge'); ?></span></a>
 	</div>
+
+	<div id='social-links' class='center'>
+		<span class='social grow'>
+			<a href='https://twitter.com/wolocodes' id='site-twitter' onclick="trackOutboundLink('wolo-twitter', 'https://twitter.com/wolocodes')"><span class='image'><?php includeSVG('', 'Twitter'); ?></span></a>
+		</span>
+		<span class='social grow'>
+			<a href='https://facebook.com/wolocodes' id='site-facebook' onclick="trackOutboundLink('wolo-facebook', 'https://facebook.com/wolocodes')"><span class='image'><?php includeSVG('', 'Facebook'); ?></span></a>
+		</span>
+		<span class='social grow'>
+			<a href='https://www.youtube.com/channel/UCnKSws8Lro8U9Ewtf1Xi5jg' id='site-youtube' onclick="trackOutboundLink('wolo-youtube', 'https://www.youtube.com/channel/UCnKSws8Lro8U9Ewtf1Xi5jg')"><span class='image'><?php includeSVG('', 'YouTube'); ?></span></a>
+		</span>
+	</div>
+	
 	<table>
 		<tr>
 			<td>About:</td>
@@ -32,17 +42,11 @@
 			<td><a class='link' href="mailto:ujjwal@wolo.codes?subject=Wolo">ujjwal@wolo.codes</a></td>
 		</tr>
 	</table>
-	<div id='social-links' class='center'>
-		<span class='social grow'>
-			<a href='https://twitter.com/wolocodes' id='site-twitter' onclick="trackOutboundLink('wolo-twitter', 'https://twitter.com/wolocodes')"><span class='image'><?php includeSVG('', 'Twitter'); ?></span></a>
-		</span>
-		<span class='social grow'>
-			<a href='https://facebook.com/wolocodes' id='site-facebook' onclick="trackOutboundLink('wolo-facebook', 'https://facebook.com/wolocodes')"><span class='image'><?php includeSVG('', 'Facebook'); ?></span></a>
-		</span>
-		<span class='social grow'>
-			<a href='https://www.youtube.com/channel/UCnKSws8Lro8U9Ewtf1Xi5jg' id='site-youtube' onclick="trackOutboundLink('wolo-youtube', 'https://www.youtube.com/channel/UCnKSws8Lro8U9Ewtf1Xi5jg')"><span class='image'><?php includeSVG('', 'YouTube'); ?></span></a>
-		</span>
-	</div>
+	
+	<ul id='info_list'>
+		<li>Tested to work with Google Chrome</li>
+	</ul>
+	
 	<div id='software_info'>
 		<div id='version'>
 			Version: <span>0.1 beta</span>

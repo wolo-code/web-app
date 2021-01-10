046912618268426431f247c04b0019db854c2107
Ujjwal Singh <ujjwalsingh@outlook.com>
2021-01-10 23:39:20 +0530

    Replaced external 'map' icon with 'launch' icon

--- Root/JS/SVGs.php
+++ Root/JS/SVGs.php
@@ -3,5 +3,5 @@ const svg_address = "<?php getSVG('', 'address'); ?>";
 const svg_copy = "<?php getSVG('', 'copy'); ?>";
 const svg_share = "<?php getSVG('', 'share'); ?>";
 const svg_link = "<?php getSVG('', 'link'); ?>";
-const svg_map = "<?php getSVG('', 'map'); ?>";
+const svg_launch = "<?php getSVG('', 'launch'); ?>";
 const svg_front = "<?php getSVG('', 'front'); ?>";

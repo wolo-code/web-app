2d42e7037c43a9581fc5b7def14ca8f7efb7e860
Ujjwal Singh <ujjwalsingh@outlook.com>
2021-06-02 02:20:31 +0530

    Moved wolo code example message from info common to info full

--- Root/HTML/Fragment/Info_full.php
+++ Root/HTML/Fragment/Info_full.php
@@ -1,4 +1,36 @@
 <div id='info_full' class='hide'>
+
+	<div id='wcode_format' class="center blue"><span>\</span> <span>City</span> <span>Word 1</span> <span>Word 2</span> <span>Word 3</span> <span>/</span></div>
+	<div id='wcode_example' class="center blue"><span>e.g.</span> <span>\</span> <span>Bengaluru</span> <span>cat</span> <span>apple</span> <span>tomato</span> <span>/</span></div>
+	<p>
+		An address in this system consists of two parts:
+	</p>
+	<ol>
+		<li>
+			<strong>City</strong> specifies the distinct city.<br>
+			Within the same city - it can be omitted.
+		</li>
+		<li>
+			The last <strong>three words</strong> specify exactly 'where' in that city.<br>
+			They are from a limited <a class='link' href='https://wcodes.org/wordlist'>list</a>  of 1,024 easy common words.
+		</li>
+	</ol>
+
+	<h2>
+		Steps
+	</h2>
+	<ul>
+		<li>
+			Enter a Wolo code address and hit the proceed button to go to that location. Or,
+		</li>
+		<li>
+			Point a location on the map to generate the corresponding Wolo code address.
+		</li>
+		<li>
+			You may also search for a particular place. Or,<br>
+			let the system pick your current location automatically.
+		</li>
+	</ul>
 	
 	<div class='message_dialog_control_container'>
 		<div class='message_dialog_control'>

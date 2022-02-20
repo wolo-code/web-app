f26564b8af7dc5fb63995cfbe7b9243df54c3461
Ujjwal Singh <ujjwalsingh@outlook.com>
2022-02-21 00:37:31 +0530

    Core algorithm moved back to client

--- Root/JS/Component/Root/Code/Code.js
+++ Root/JS/Component/Root/Code/Code.js
@@ -1,103 +1,102 @@
 // const FUNCTIONS_BASE_URL;
-// var curEncRequestId;
-// var curDecRequestId;
 // var curAddCityRequestId;
 
-function encode_(city, position, session_id) {
-	if(typeof session_id != 'undefined' && session_id == encode_session_id)
-		code_city = city;
-	else
-		code_city = null;
-	var http = new XMLHttpRequest();
-	http.open('POST', FUNCTIONS_BASE_URL+'/'+'encode', true);
+// 2^(10+5)
+const N = 32768;
+const A = 6378137;
+const B = 6356752.314140;
+const E_SQ = (A*A-B*B)/(A*A);
+const DEG_RAD = Math.PI/180;
 
-	http.setRequestHeader('Content-type', 'application/json');
-	http.setRequestHeader('version', '1');
-	http.requestId = ++curEncRequestId;
+function lat_span_half(lat) {
+	const lat_r = DEG_RAD*lat;
+	const x = Math.sqrt(1-E_SQ*Math.sin(lat_r)*Math.sin(lat_r));
+	return Math.abs((x*x*x)/(DEG_RAD*A*(1-E_SQ)));
+}
 
-	pushLoader();
-	http.onreadystatechange = function() {
-		if(http.readyState == 4) {
-			if(http.requestId == curEncRequestId)
-				if(typeof session_id != 'undefined' && session_id == encode_session_id) {
-					popLoader();
-					if(http.status == 200) {
-						setCodeWords(http.responseText, city, position);
-					}
-					else if(http.status == 416) {
-						notInRange(position);
-					}
-				}
-		}
+function lng_span_half(lat) {
+	const lat_r = DEG_RAD*lat;
+	return Math.abs(Math.sqrt(1-E_SQ*Math.sin(lat_r)*Math.sin(lat_r))/(DEG_RAD*A*Math.cos(lat_r)));
+}
+
+function encodeData(value, d) {
+	const i = Math.round(value/d);
+	if(i < 0 || i > N) {
+		console.log("Error: Out of data limit");
+		console.log("Value: " + value);
+		console.log("d: " + d);
 	}
+	else
+		return i;
+}
 
-	http.send( stringifyEncodeData(city.center, position) );
-	return '';
+function decodeData(data, d) {
+	return data*d;
+}
+
+function getCityBegin(cityCenter) {
+	const lat = cityCenter.lat - lat_span_half(cityCenter.lat)*N;
+	const lng = cityCenter.lng - lng_span_half(cityCenter.lat)*N;
+	return {'lat': lat, 'lng': lng};
+}
+
+function encode_(city, position) {
+	const code = encode__(getCityBegin(city.center), position);
+	for(var i of code)
+		if(i < 0 || i > 1023) {
+			console.log("Error: Out of WCode index limit");
+			noCity(position);
+			notification_top.classList.remove('hide');
+			wait_loader.classList.add('hide');
+		}
+	setCodeWords(code, city, position);
 }
 
 function setCodeWords(code, city, position) {
 	var message = [];
-	var object = JSON.parse(code).code;
 
-	for(const i of object)
+	for(const i of code)
 		message.push(wordList.getWord(i));
 
 	setCode(city, message, position);
 }
 
-function stringifyEncodeData(city_center, position) {
-	var object = {};
-	object['city_center'] = city_center;
-	object['position'] = position;
-	return JSON.stringify(object);
-}
-
 function decode_(city, code) {
-	code_city = city;
-	var http = new XMLHttpRequest();
-	http.open('POST', FUNCTIONS_BASE_URL+'/'+'decode', true);
-
-	http.setRequestHeader('Content-type', 'application/json');
-	http.setRequestHeader('version', '1');
-	http.requestId = ++curDecRequestId;
-
-	pushLoader();
-	http.onreadystatechange = function() {
-		if(http.readyState == 4 && http.status == 200) {
-			if(http.requestId == curDecRequestId) {
-				setCodeCoord(city, http.responseText, code);
-				notification_top.classList.add('hide');
-				popLoader();
-			}
-		}
-	}
-
 	var data = [];
 	data[0] = wordList.indexOf(code[0]);
 	data[1] = wordList.indexOf(code[1]);
 	data[2] = wordList.indexOf(code[2]);
-	http.send( stringifyDecodeData(city.center, data) );
+	var position = decode__(getCityBegin(city.center), data)	;
+	setCodeCoord(city, position, code);
+	notification_top.classList.add('hide');
+	wait_loader.classList.add('hide');
 }
 
-function stringifyDecodeData(city_center, code) {
-	var object = {};
-	object['city_center'] = city_center;
-	object['code'] = code;
-	return JSON.stringify(object);
+function encode__(city_begin, position) {
+	const lat_diff = encodeData(position.lat - city_begin.lat, lat_span_half(city_begin.lat)*2);
+	const lng_diff = encodeData(position.lng - city_begin.lng, lng_span_half(city_begin.lat)*2);
+	const word_index_1 = lat_diff >> 5;
+	const word_index_2 = lng_diff >> 5;
+	const word_index_3 = (lat_diff & 0x001F) << 5 | (lng_diff & 0x001F);
+	const code = [word_index_1, word_index_2, word_index_3];
+
+	return code;
 }
 
-function setCodeCoord(city, codeIndex, code) {
-	var latLng = JSON.parse(codeIndex);
-	if(initWCode_jumpToMap) {
-		initWCode_jumpToMap = false;
-		window.location.replace(getIntentURL(latLng, city.name + ' ' + code.join(' ')));
-	}
-	else {
-		if(initWCode_jump_ask) {
-			initWCode_jump_ask = false;
-			external_show(latLng, city.name, code.join(' '));
-		}
-		getAddress(latLng);
-		focus__(city, latLng, code);
-	}
+function decode__(city_begin, code) {
+	const word_index_1 = code[0];
+	const word_index_2 = code[1];
+	const word_index_3 = code[2];
+	const lat_diff_bin = word_index_1 << 5 | word_index_3 >> 5;
+	const lng_diff_bin = word_index_2 << 5 | word_index_3 & 0x001F;
+	const lat_diff = decodeData(lat_diff_bin, lat_span_half(city_begin.lat)*2);
+	const lng_diff = decodeData(lng_diff_bin, lng_span_half(city_begin.lat)*2);
+	const lat = city_begin.lat + lat_diff;
+	const lng = city_begin.lng + lng_diff;
+
+	return({"lat":lat, "lng":lng});
+}
+
+function setCodeCoord(city, position, code) {
+	focus__(city, position, code);
 }

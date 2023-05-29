7326000eb2dc02e583c2bdb12363b75939783280
Ujjwal Singh <ujjwalsingh@outlook.com>
2023-05-30 02:04:34 +0530

    Removed - stray: '.jpg' from twitter tag content

--- Root/Framework/HTML/Fragment/Twitter_meta.php
+++ Root/Framework/HTML/Fragment/Twitter_meta.php
@@ -3,4 +3,4 @@
 <meta name="twitter:title" content="<?php echo getComponentTitle($id) ?>">
 <meta name="twitter:description" content="<?php echo getComponentDesc($id) ?>">
 <meta name="twitter:image" content="<?php echo $config['base_url'].'/'.getComponentMetaImage($id) ?>">
-<meta name="twitter:creator" content="<?php echo $config['twitter_author'] ?>.jpg">
+<meta name="twitter:creator" content="<?php echo $config['twitter_author'] ?>">

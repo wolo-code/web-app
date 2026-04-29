e559a0aeb69a7f96dd467680904ff9db86be5a79
Ujjwal Singh <ujjwalsingh@outlook.com>
2026-04-30 01:13:20 +0530

    Guard app mode background sync

--- Root/JS/Component/Root/Map.js
+++ Root/JS/Component/Root/Map.js
@@ -8,6 +8,7 @@ var APP_MODE_BACKGROUND = {
 	map: '#60d0e5',
 	satellite: '#1b2f62'
 };
+var APP_MODE_BACKGROUND_DEFAULT = '#efefef';
 
 // const INCORRECT_WCODE;
 // const MESSAGE_LOADING;
@@ -24,8 +25,10 @@ function getAppMode() {
 
 function syncAppModeBackground() {
 	var mode = getAppMode();
-	var background = APP_MODE_BACKGROUND[mode];
+	var background = typeof APP_MODE_BACKGROUND == 'object' && APP_MODE_BACKGROUND ? APP_MODE_BACKGROUND[mode] : null;
 	var themeColor = document.querySelector('meta[name="theme-color"]');
+	if(!background)
+		background = APP_MODE_BACKGROUND_DEFAULT || '#efefef';
 	document.body.dataset.appMode = mode;
 	if(themeColor)
 		themeColor.setAttribute('content', background);
